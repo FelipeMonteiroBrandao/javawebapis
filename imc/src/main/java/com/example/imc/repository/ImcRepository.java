af77adb081023845dc1e63ab0d44053670d4aa15
FelipeMonteiroBrandao <87206521+FelipeMonteiroBrandao@users.noreply.github.com>
2022-02-23 22:03:51 -0300

    Delete imc directory

--- imc/src/main/java/com/example/imc/repository/ImcRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.imc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.imc.models.Imc;
-
-public interface ImcRepository extends JpaRepository<Imc, Long> {
-
-}
